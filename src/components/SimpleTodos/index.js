b578bbd778352c99ec6d5c49c0913a7524beb826
sainathsingh08 <sainathsingh08@gmail.com>
2024-04-17 10:31:11 +0000

    simple

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+
+import TodoItem from '../TodoItem'
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+// Write your code here
+class SimpleTodos extends Component {
+  state = {
+    todList: initialTodosList,
+  }
+
+  deleteTodo = id => {
+    const {todList} = this.state
+    const {updatedTodoList} = todList.filter(eachTodo => eachTodo.id !== id)
+
+    this.setState({
+      todList: updatedTodoList,
+    })
+  }
+  render() {
+    const {todList} = this.state
+
+    return (
+      <ul className="todo-list">
+        {todList.map(eachTodo => (
+          <TodoItem
+            key={eachTodo.id}
+            todoDetails ={eachTodo}
+            deleteTodo={this.deleteTodo}
+        ))}
+      </ul>
+    )
+  }
+}
